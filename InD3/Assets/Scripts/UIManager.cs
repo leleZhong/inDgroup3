1e818941b687d93843221cd33e7b45b7e175e266
immy0815 <keycys11@naver.com>
2024-05-02 20:08:32 +0900

    24.05.02

--- InD3/Assets/Scripts/UIManager.cs
+++ InD3/Assets/Scripts/UIManager.cs
@@ -8,6 +8,9 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     public GameManager gameManager;
+
+    public GameObject roomScene;
+    public GameObject yardScene;
     
     //==============================================[ 안내 메시지 ]====================================================
     // 안내 메시지
@@ -28,14 +31,15 @@ public class UIManager : MonoBehaviour
     //==============================================[ 놀이 UI ]======================================================
     //  놀이 상자 변수
     [Header("----[ Play UI ]")]
-    public Sprite[] playTypeSprites;
-    public GameObject curGameStateGroup;
-    public GameObject curGameStateObj;
-    public Image curGameImage;
+    public GameObject playBoxPanel;  // 놀이 상자 패널
+    public Sprite[] playTypeSprites; // 구현된 놀이 종류 스프라이트
+    public GameObject curGameStateGroup; // 현재 게임 순서에 따라 바뀌는 하단 점 상태 표시 그룹
+    public GameObject curGameStatePrefab; // 하단 점 프리펩
+    public Image curGameImage; // 현재 놀이 상자에 표시되는 게임 이미지
     
     //===============================================[ 시간 계산 ]====================================================
     
-    public TMP_Text careCoolTime;
+    public TMP_Text careCoolTime; // 게임 쿨타임 텍스트
 
     //==============================================[ 케어 지수, 정도, UI ]============================================
     [Header("----[ Care UI ]")]
@@ -51,14 +55,25 @@ public class UIManager : MonoBehaviour
     public Button playBtn;  // 놀기 버튼
 
     // public GameObject shopPanel; // 상점 패널
-    public GameObject foodBoxPanel;  // 먹이 상자 패널
-    public GameObject playBoxPanel;  // 놀이 상자 패널
+    public GameObject toyPanel;
+    public TMP_Text toyInfoText; // 장난감 정보 텍스트
+    public TMP_Text toyNameText; // 장난감 이름 텍스트
+    public Image[] toyItemBtn; // 보이는 장난감 이미지
+    public Sprite[] toyItemO; // 장난감 있을 때 이미지
+    public Sprite[] toyItemX; // 장난감 없을 때 이미지
+    public Sprite[] toyItemSelected; // 장난감 선택시 이미지
 
-    public Image[] expStates;
-    
+
+    public Image[] expStates; // 경험치 상태 - 게임오브젝트로 수정해
+    public GameObject expStateGroup; // 경험치 상태 오브젝트 그룹
+    public GameObject expStatePrefab; // 경험치 상태 오브젝트 프리펩
+    public TMP_Text expStateTMP; // 경험치 상태 텍스트 99/99
+
+    public GameObject dkbSettingBtn; // 도깨비 세팅 버튼
+
     //==============================================[ 기타 ]==========================================================
 
-    private bool _isPopUp;
+    public bool isPopUp;
     
     // ==============================================================================================================
 
@@ -67,8 +82,11 @@ public class UIManager : MonoBehaviour
         // 필요 UI를 제외한 나머지 전부 비활성화하고 시작
         infoMessage.SetActive(false);
         foodPanel.SetActive(false);
-        foodBoxPanel.SetActive(false);
         playBoxPanel.SetActive(false);
+        
+        
+        AddCurExpState();
+        ChangeExpText();
     }
 
     // ==============================================================================================================
@@ -76,7 +94,7 @@ public class UIManager : MonoBehaviour
     // 먹이 함수 1. 배부를 때는 밥X, 아닐 때 먹이 상자 Enable
     public void FoodPanelButton()
     {
-        if(_isPopUp)
+        if(isPopUp)
             return;
         
         for(int i = 0; i < foodCountText.Length; i++)
@@ -85,14 +103,14 @@ public class UIManager : MonoBehaviour
         }
 
         foodPanel.SetActive(true);
-        _isPopUp = true;
+        isPopUp = true;
     }
 
     // 먹이 함수 2. 밥 주기 취소 (패널 Disable, 먹이 상자 초기화)
     public void FoodBoxCancel()
     {
         foodPanel.SetActive(false);
-        _isPopUp = false;
+        isPopUp = false;
         for (int i = 0; i < foodItem.Length; i++)
         {
             foodItem[i].color = new Color(1, 1, 1, 1);
@@ -103,14 +121,14 @@ public class UIManager : MonoBehaviour
     }
 
     // 먹이 함수 3. 먹이 눌렀을 때 정보 표출, 하나의 먹이만 선택 가능
-    public void FoodBoxItem(int foodindex)
+    public void FoodBoxItem(int foodIndex)
     {
         for (int i = 0; i < foodItem.Length; i++)
         {
-            if (i == foodindex)
+            if (i == foodIndex)
             {
-                foodItem[foodindex].color = new Color(0.5f, 0.5f, 0.5f, 1);
-                switch (foodindex)
+                foodItem[foodIndex].color = new Color(0.5f, 0.5f, 0.5f, 1);
+                switch (foodIndex)
                 {
                     case 0:
                         foodNameText.text = "0번 음식";
@@ -146,13 +164,13 @@ public class UIManager : MonoBehaviour
     {
         if (gameManager.feedAmount >= 100)
         {
-            StartCoroutine(GameinfoMessage("배부른 것 같습니다."));
+            StartCoroutine(GameInfoMessage("배부른 것 같습니다."));
             return;
         }
 
         if (gameManager.foodType == 100)
         {
-            StartCoroutine(GameinfoMessage("줄 음식을 선택해주세요."));
+            StartCoroutine(GameInfoMessage("줄 음식을 선택해주세요."));
             return;
         }
 
@@ -165,7 +183,7 @@ public class UIManager : MonoBehaviour
         }
         else
         {
-            StartCoroutine(GameinfoMessage("음식을 보유하고 있지 않습니다."));
+            StartCoroutine(GameInfoMessage("음식을 보유하고 있지 않습니다."));
         }
     }
     
@@ -174,12 +192,12 @@ public class UIManager : MonoBehaviour
     // 케어 함수 Play. 
     public void PlayBoxButton()
     {
-        if(_isPopUp)
+        if(isPopUp)
             return;
         
-        if (gameManager.maxPlayCount <= 0)
+        if (gameManager.playCount <= 0)
         {
-            StartCoroutine(GameinfoMessage("피곤해 보입니다."));
+            StartCoroutine(GameInfoMessage("피곤해 보입니다."));
         }
         else
         {
@@ -193,21 +211,21 @@ public class UIManager : MonoBehaviour
                     ChangeCurGameState(curGameIndex);
                 }
             }
-            _isPopUp = true;
+            isPopUp = true;
         }
     }
     
     public void PlayBoxAccept()
     {
-        gameManager.maxPlayCount--;
+        gameManager.playCount--;
         playBoxPanel.SetActive(false);
-        _isPopUp = false;
+        isPopUp = false;
     }
     
     public void PlayBoxCancel()
     {
         playBoxPanel.SetActive(false);
-        _isPopUp = false;
+        isPopUp = false;
     }
 
     public void NextGameBtn()
@@ -246,7 +264,7 @@ public class UIManager : MonoBehaviour
     {
         if(curGameStateGroup.GetComponentsInChildren<Image>().Length == playTypeSprites.Length)
             return;
-        GameObject newGameState = Instantiate(curGameStateObj, curGameStateGroup.transform);
+        GameObject newGameState = Instantiate(curGameStatePrefab, curGameStateGroup.transform);
         newGameState.transform.position = curGameStateGroup.transform.position;
         newGameState.transform.rotation = curGameStateGroup.transform.rotation;
         
@@ -271,18 +289,44 @@ public class UIManager : MonoBehaviour
     
     // ==============================================================================================================
 
-    public void EXPStateUpdate()
+    public void ExpStateUpdate()
     {
-        for (int i = 1; i < gameManager.exp; i++)
+        for (int i = 0; i < gameManager.exp; i++)
         {
             expStates[i].color = Color.yellow;;
+            ChangeExpText();
         }
+    }
+
+    public void ExpStateReset()
+    {
+        for (int i = 0; i < gameManager.exp; i++)
+        {
+            expStates[i].color = Color.black;;
+        }
+        AddCurExpState();
+    }
+    
+    void AddCurExpState()
+    {
+        for (int i = 0; i < gameManager.expRange[gameManager.growthLevel]; i++)
+        {
+            GameObject newExpState = Instantiate(expStatePrefab, expStateGroup.transform);
+            newExpState.transform.position = expStateGroup.transform.position;
+            newExpState.transform.rotation = expStateGroup.transform.rotation;
+        }
+        expStates = expStateGroup.GetComponentsInChildren<Image>();
+    }
+    
+    public void ChangeExpText()
+    {
+        expStateTMP.text = string.Format("{0:D2}:{1:D2}", gameManager.exp, gameManager.expRange[gameManager.growthLevel]);
     }
     
     // ==============================================================================================================
     
     // 게임 내 안내메시지
-    IEnumerator GameinfoMessage(string infoMessageStr)
+    private IEnumerator GameInfoMessage(string infoMessageStr)
     {
         infoMessage.SetActive(true);
         infoMessageText.text = infoMessageStr;
@@ -297,7 +341,7 @@ public class UIManager : MonoBehaviour
         StartCoroutine(CareAmountTextNumber(careAmountType));
     }
 
-    IEnumerator CareAmountTextNumber(int careAmountType)
+    private IEnumerator CareAmountTextNumber(int careAmountType)
     {
         // 각 케어 지수 아이콘 이미지 알파값 낮추고 현재 지수 텍스트 활성화
         switch(careAmountType)
@@ -330,5 +374,109 @@ public class UIManager : MonoBehaviour
         }
     }
     
-   
+
+    // ==============================================================================================================
+    
+    public void SetDokkaebiButton()
+    {
+        if(isPopUp)
+            return;
+
+        toyPanel.SetActive(true);
+        isPopUp = true;
+    }
+    
+
+    // 
+    public void ToyBoxCancel()
+    {
+        toyPanel.SetActive(false);
+        isPopUp = false;
+        toyNameText.text = "장난감 이름";
+        toyInfoText.text = "장난감 선택 시 정보를 표시합니다.";
+        if(gameManager.toyType != 100)
+            toyItemBtn[gameManager.toyType].sprite = toyItemO[gameManager.toyType];
+        gameManager.toyType = 100;
+    }
+
+    // 
+    public void ToyBoxItem(int toyIndex)
+    {
+        for (int i = 0; i < toyItemBtn.Length; i++)
+        {
+            if (i == toyIndex)
+            {
+                if (toyItemBtn[i].sprite == toyItemX[i])
+                {
+                    StartCoroutine(GameInfoMessage("장난감을 먼저 구매해주세요."));
+                    return;
+                }
+                
+                if (gameManager.toyType != 100)
+                {
+                    toyItemBtn[gameManager.toyType].sprite = toyItemO[gameManager.toyType];
+                }
+                toyItemBtn[toyIndex].sprite = toyItemSelected[toyIndex];
+                gameManager.toyType = toyIndex;
+                switch (gameManager.toyType)
+                {
+                    case 0:
+                        toyNameText.text = "0번 장난감";
+                        toyInfoText.text = "0번이에유";
+                        break;
+                    case 1:
+                        toyNameText.text = "1번 장난감";
+                        toyInfoText.text = "1번이에유";
+                        break;
+                    case 2:
+                        toyNameText.text = "2번 장난감";
+                        toyInfoText.text = "2번이에유";
+                        break;
+                    case 3:
+                        toyNameText.text = "3번 장난감";
+                        toyInfoText.text = "3번이에유";
+                        break;
+                }
+            }
+        }
+    }
+
+    // 
+    public void ToyBoxAccept()
+    {
+        if (gameManager.toyType == 100)
+        {
+            StartCoroutine(GameInfoMessage("놓을 장난감을 선택해주세요."));
+            return;
+        }
+        
+        toyItemBtn[gameManager.toyType].sprite = toyItemX[gameManager.toyType];
+        
+        // 장난감 패널 닫기
+        toyPanel.SetActive(false);
+        toyItemBtn[gameManager.toyType].sprite = toyItemO[gameManager.toyType];
+        
+        // 도깨비 장난감 설치
+        dkbSettingBtn.SetActive(false);
+        gameManager.isGrowStart = true;
+        gameManager.dkbToy.SetActive(true);
+        SpriteRenderer dokkaebiToySpriteRenderer = gameManager.dkbToy.GetComponent<SpriteRenderer>();
+        dokkaebiToySpriteRenderer.sprite = toyItemO[gameManager.toyType];
+    }
+
+    public void GoYard()
+    {
+        roomScene.SetActive(false);
+        yardScene.SetActive(true);
+        gameManager.collectionGroup.SetActive(true);
+    }
+
+    public void GoRoom()
+    {
+        roomScene.SetActive(true);
+        yardScene.SetActive(false);
+        gameManager.collectionGroup.SetActive(false);
+    }
+
+    
 }
